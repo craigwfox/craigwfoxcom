0d297548b7c3292b756f21e5db7cbba0eaa4bc7e
craigwfox <craig@craigwfox.com>
2014-01-06 14:36:06 -0600

    Fixed js link in the footer.html

--- assets/jsbin/site.js
+++ assets/jsbin/site.js
@@ -11,12 +11,5 @@ By Craig Fox @ craigwfox.com
 https://github.com/craigwfox/Simple-Starter-Theme.git
 ******************************************************* */
 $(function(){
-	$("ul.dropdown li").hover(function(){
-		$(this).addClass("hover");
-		$('ul:first',this).css('visibility', 'visible');
-	}, function(){
-		$(this).removeClass("hover");
-		$('ul:first',this).css('visibility', 'hidden');
-	});
-	$("ul.dropdown li ul li:has(ul)").find("a:first").append(" &raquo; ");
+	
 });
