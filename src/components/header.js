52839adeb0d8501a99561e5f0796a8dd1b33fb56
Craig Fox <craig@craigwfox.com>
2021-11-22 23:29:10 -0600

    removing old data

--- src/components/header.js
+++ src/components/header.js
@@ -2,19 +2,12 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
-import headerLogo from "../images/popcorn.svg"
-
 const Header = ({ siteTitle, siteDescription }) => (
   <header className="site-header">
     <h1 className="site-logo">
-      <img src={headerLogo} alt="A bucket of popcorn" className="site-logo" />
       <span>{siteTitle}</span>
     </h1>
     <h2>{siteDescription}</h2>
-    <nav className="nav-main">
-      <Link to="/">Home</Link>
-      <Link to="/support/about">About</Link>
-    </nav>
   </header>
 )
 
