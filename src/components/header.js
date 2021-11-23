91460f4de842404f463e2e54bc93a1816d8a679c
Craig Fox <craig@craigwfox.com>
2021-11-23 10:48:00 -0600

    Added various logo colors for testing what I like

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
-import svgCraigLogo from "../images/craig-logo.svg"
+import svgCraigLogo from "../images/craig-logo-red.svg"
 
 const Header = ({ siteDescription }) => (
   <header className="site-header">
