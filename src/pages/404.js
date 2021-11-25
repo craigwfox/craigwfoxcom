e6df4b9c30af2f116290f37e5f1cdf3a63516c3a
Craig Fox <craig@craigwfox.com>
2021-11-24 22:42:51 -0600

    Updated 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,24 @@
 import * as React from "react"
+import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <Seo title="💥 404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <section className="section">
+      <Seo title="💥 404: Not found" />
+      <h1>404: Not Found</h1>
+      <p>
+        You just hit a route that doesn&#39;t exist... have a picture of my
+        cats.
+      </p>
+      <StaticImage
+        className="image-border"
+        src="../images/kitty-apples-box.webp"
+        width={500}
+      />
+    </section>
   </Layout>
 )
 
