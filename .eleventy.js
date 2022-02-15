958407a4a5b0ad8f9369e98b7986a1195c49df3e
Craig Fox <cwfox@arkbluecross.com>
2022-02-15 13:21:34 -0600

    setup scss

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,16 @@
 const Image = require("@11ty/eleventy-img")
+const { clientScript } = require("browser-sync/dist/connect-utils")
 
-async function imageShortcode(src, alt, sizes) {
+async function imageShortcode(src, alt, sizes, cls) {
   let metadata = await Image(src, {
-    widths: [300, 600],
+    widths: [25, 300, 600],
     svgShortCircuit: true,
     formats: ["avif", "webp", "jpeg", "svg"],
     outputDir: "./dist/img/",
   })
 
   let imageAttributes = {
+    class: cls,
     alt,
     sizes,
     loading: "lazy",
@@ -23,6 +25,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode)
   eleventyConfig.addLiquidShortcode("image", imageShortcode)
   eleventyConfig.addJavaScriptFunction("image", imageShortcode)
+  eleventyConfig.addWatchTarget("./src/_includes/css/")
+  eleventyConfig.addPassthroughCopy("./src/css/")
+  eleventyConfig.setUseGitIgnore(false)
 
   return {
     dir: {
