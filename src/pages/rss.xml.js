4c09c5ba504126a03fa6965c37c0a014dd43e1b1
Craig Fox <craig@craigwfox.com>
2023-03-07 19:51:05 -0600

    updated RSS to show all post content

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,21 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import rss from "@astrojs/rss"
+import { getCollection } from "astro:content"
+import { SITE_TITLE, SITE_DESCRIPTION } from "../consts"
+import sanitizeHtml from "sanitize-html"
+import MarkdownIt from "markdown-it"
+const parser = new MarkdownIt()
 
 export async function get(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
-	});
+  const posts = await getCollection("blog")
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: posts.map(post => ({
+      ...post.data,
+      title: post.data.title,
+      content: sanitizeHtml(parser.render(post.body)),
+      link: `/blog/${post.slug}/`,
+    })),
+  })
 }
