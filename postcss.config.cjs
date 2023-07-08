c0602c883ecc2ad7072c18b8564756ca9767cc3c
Craig Fox <craig@craigwfox.com>
2023-07-08 18:49:15 -0500

    base styling

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,19 +3,24 @@ const cssnano = require("cssnano")
 const imports = require("postcss-import")
 const postcssPresetEnv = require("postcss-preset-env")
 
+
 const config = {
   plugins: [
     imports,
-    autoprefixer,
     cssnano,
     postcssPresetEnv({
-      stage: 3,
+      stage: 2,
       features: {
         "nesting-rules": true,
         "custom-media-queries": true,
         "media-query-ranges": true,
+        "color-function": true,
+        "color-functional-notation": true,
+        "color-mix": true,
+        "oklab-function": true,
       },
     }),
+    autoprefixer,
   ],
 }
 
