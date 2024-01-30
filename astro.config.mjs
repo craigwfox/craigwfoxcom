5f302407172a18eddad0d5716f46d027eee18bf9
Craig Fox <craig@craigwfox.com>
2024-01-30 17:45:56 -0600

    testing ssg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
-import { defineConfig } from "astro/config"
+import { defineConfig, sharpImageService } from "astro/config"
 import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import svelte from "@astrojs/svelte"
-import netlify from "@astrojs/netlify"
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify(),
   site: "https://craigwfox.com",
   markdown: {
     syntaxHighlight: "prism",
