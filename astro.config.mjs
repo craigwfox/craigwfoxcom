271353c25d7bed20bf52df49e8cc2718cf2e0dc3
Craig Fox <craig@craigwfox.com>
2024-01-30 17:49:06 -0600

    swapping back to netlify / server

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig, sharpImageService } from "astro/config"
 import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import svelte from "@astrojs/svelte"
+import netlify from "@astrojs/netlify"
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
+  output: "server",
+  adapter: netlify(),
   site: "https://craigwfox.com",
   markdown: {
     syntaxHighlight: "prism",
