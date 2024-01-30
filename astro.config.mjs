720072566bb2d7f7ba4d33083c156e04ccdbc7cb
Craig Fox <craig@craigwfox.com>
2024-01-30 17:47:41 -0600

    swapping to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import svelte from "@astrojs/svelte"
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "static",
   site: "https://craigwfox.com",
   markdown: {
     syntaxHighlight: "prism",
