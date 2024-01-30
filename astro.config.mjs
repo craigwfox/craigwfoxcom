5cffeb69b98f622261b77c9a7cbc2048d9fa1fa2
Craig Fox <craig@craigwfox.com>
2024-01-30 17:57:31 -0600

    updated deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig, sharpImageService } from "astro/config"
+import { defineConfig } from "astro/config"
 import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import svelte from "@astrojs/svelte"
