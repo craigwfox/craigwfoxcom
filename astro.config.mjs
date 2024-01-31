46e32e2d78d63a893edd71370c633763366bd61f
Craig Fox <craig@craigwfox.com>
2024-01-30 18:40:00 -0600

    who knows

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config"
 import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import svelte from "@astrojs/svelte"
-import netlify from "@astrojs/netlify/functions"
+import netlify from "@astrojs/netlify"
 
 // https://astro.build/config
 export default defineConfig({
