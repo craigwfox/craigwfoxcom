66ad6457ea3a84ac74655b4f7b88204f14e36532
Craig Fox <craig@craigwfox.com>
2024-02-13 10:49:16 -0600

    improving image loading for gallery component

--- src/components/ImageGallery.svelte
+++ src/components/ImageGallery.svelte
@@ -1,10 +1,9 @@
 <script>
-  export let imageArry = []
-  let imageCounter = 0
+  export let imageId = null
+  export let imageAlt = null
 
   function showImage(event) {
     const button = event.currentTarget
-    console.log(button)
     const buttonId = button.getAttribute("aria-controls")
     const figure = document.getElementById(buttonId)
 
@@ -34,52 +33,50 @@
       .setAttribute("aria-expanded", "false")
     figure.setAttribute("hidden", true)
   }
+
+  const images = import.meta.glob("/src/images/posts/**/*")
 </script>
 
-<div class="image-gallery">
-  {#each imageArry as image, i}
-    <div class="image-gallery__item">
-      <button
-        on:click={showImage}
-        type="button"
-        aria-controls={`gallery-image-${i}`}
-        aria-expanded="false"
-        class="open"
+<div class="image-gallery__item">
+  <button
+    on:click={showImage}
+    type="button"
+    aria-controls={`gallery-image-${imageId}`}
+    aria-expanded="false"
+    class="open"
+  >
+    <slot name="gallery-btn" />
+  </button>
+  <div id={`gallery-image-${imageId}`} hidden>
+    <button
+      on:click={closeImage}
+      type="button"
+      aria-controls={`gallery-image-${imageId}`}
+      aria-expanded="false"
+      class="close"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentcolor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        ><circle cx="12" cy="12" r="10"></circle><line
+          x1="15"
+          y1="9"
+          x2="9"
+          y2="15"
+        ></line><line x1="9" y1="9" x2="15" y2="15"></line></svg
       >
-        <img src={image.path} alt={image.alt} />
-      </button>
-      <div id={`gallery-image-${i}`} hidden>
-        <button
-          on:click={closeImage}
-          type="button"
-          aria-controls={`gallery-image-${i}`}
-          aria-expanded="false"
-          class="close"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentcolor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            ><circle cx="12" cy="12" r="10"></circle><line
-              x1="15"
-              y1="9"
-              x2="9"
-              y2="15"
-            ></line><line x1="9" y1="9" x2="15" y2="15"></line></svg
-          >
-          <span class="visually-hidden">Close</span>
-        </button>
-        <figure>
-          <img src={image.path} alt={image.alt} />
-          <figcaption>{image.alt}</figcaption>
-        </figure>
-      </div>
-    </div>
-  {/each}
+      <span class="visually-hidden">Close</span>
+    </button>
+    <figure>
+      <slot name="gallery-full" />
+      <figcaption>{imageAlt}</figcaption>
+    </figure>
+  </div>
 </div>
