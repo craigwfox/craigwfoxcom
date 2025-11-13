2192aa77d49145f99dce85ae12f2c60ce6a5ea0f
Craig Fox <craig@craigwfox.com>
2025-11-13 16:41:35 -0600

    More sitemap fixes for inprogress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config"
 import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
-
 import vue from "@astrojs/vue"
 import * as compiler from "vue/compiler-sfc"
 
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,3 +1,4 @@
+import { glob } from "astro/loaders"
 import { defineCollection, z } from "astro:content"
 
 function getYear(startDate: Date, endDate: Date) {
